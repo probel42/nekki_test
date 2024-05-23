7b6e34b40158afc556f4ff46d86e62c743862d12
probel42 <bel3i@yandex.ru>
2024-05-23 13:45:28 +0500

    clear script fix

--- clear.sh
+++ clear.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker rmi ibelan_nekki-backend
-#docker rmi ibelan_nekki-backend_build
+docker rmi ibelan_nekki_backend
+#docker rmi ibelan_nekki_backend_build
 
 docker volume rm ibelan_nekki_backend_build_volume
 
