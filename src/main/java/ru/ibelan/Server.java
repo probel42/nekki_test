8893a28a38c8b26d6b855bf6173d4701078180c7
probel42 <bel3i@yandex.ru>
2024-05-23 10:00:30 +0500

    little fix

--- src/main/java/ru/ibelan/Server.java
+++ src/main/java/ru/ibelan/Server.java
@@ -48,13 +48,7 @@ public class Server {
 		} catch (Exception e) {
 			log.error(e.getMessage());
 		} finally {
-			connectionPool.forEach(con -> {
-				try {
-					con.close();
-				} catch (Exception e) {
-					log.error(e.getMessage());
-				}
-			});
+			connectionPool.forEach(con -> closeConnection(con, false));
 		}
 	}
 
